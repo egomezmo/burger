c04f051d10460b3cfb00cd5ecdebe9fe96587dc0
Enrique Gomez <egomezmo@prodigy.net.mx>
2018-11-24 14:54:59 -0600

    final review

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,15 +1,14 @@
 var express = require("express");
-
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -17,10 +16,10 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/cats", function(req, res) {
-  cat.create([
-    "name", "sleepy"
+  burger.create([
+    "burger_name", "devoured"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.burger_name, 1
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -32,8 +31,8 @@ router.put("/api/cats/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
+  burger.update({
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
@@ -47,7 +46,7 @@ router.put("/api/cats/:id", function(req, res) {
 router.delete("/api/cats/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,11 +1,11 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-sleep").on("click", function(event) {
+$(function () {
+  $(".change-sleep").on("click", function (event) {
     var id = $(this).data("id");
     var newSleep = $(this).data("newsleep");
 
     var newSleepState = {
-      sleepy: newSleep
+      devoured: newSleep
     };
 
     // Send the PUT request.
@@ -13,7 +13,7 @@ $(function() {
       type: "PUT",
       data: newSleepState
     }).then(
-      function() {
+      function () {
         console.log("changed sleep to", newSleep);
         // Reload the page to get the updated list
         location.reload();
@@ -21,13 +21,19 @@ $(function() {
     );
   });
 
-  $(".create-form").on("submit", function(event) {
+
+
+  // aqui debe estar el problema
+  $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newCat = {
-      name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+
+      burger_name: $("#ca").val().trim(),
+      
+ 
+
     };
 
     // Send the POST request.
@@ -35,7 +41,7 @@ $(function() {
       type: "POST",
       data: newCat
     }).then(
-      function() {
+      function () {
         console.log("created new cat");
         // Reload the page to get the updated list
         location.reload();
@@ -43,14 +49,19 @@ $(function() {
     );
   });
 
-  $(".delete-cat").on("click", function(event) {
+  // aqui debe estar el problema
+
+
+
+
+  $(".delete-cat").on("click", function (event) {
     var id = $(this).data("id");
 
     // Send the DELETE request.
     $.ajax("/api/cats/" + id, {
       type: "DELETE"
     }).then(
-      function() {
+      function () {
         console.log("deleted cat", id);
         // Reload the page to get the updated list
         location.reload();
